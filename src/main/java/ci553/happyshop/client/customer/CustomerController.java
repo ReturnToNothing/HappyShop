8ca0b8da8c7368d31c5fb46f90b0843e9f83de94
Return <131034915+ReturnToNothing@users.noreply.github.com>
2025-11-03 16:28:43 +0000

    + Added documentary about the Controller's method and purpose

--- src/main/java/ci553/happyshop/client/customer/CustomerController.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerController.java
@@ -1,29 +1,55 @@
 package ci553.happyshop.client.customer;
+import ci553.happyshop.catalogue.Product;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
+/**
+ * CustomerController acts as a dispatcher for the CustomerView; converting actions into executions towards the CustomerModel.
+ */
 public class CustomerController {
     public CustomerModel cusModel;
-
-    public void doAction(String action) throws SQLException, IOException {
+    // Literally forgotten that java's index starts 0 not 1...
+    /**
+     * Overloading method of doAction, dispatches customer action.
+     * @param action The name of the action to perform.
+     * @param object The arguments required for the action.
+     * @throws SQLException if a database operation fails.
+     * @throws IOException  if an I/O operation fails.
+     */
+    public void doAction(String action, Object[] object) throws SQLException, IOException {
         switch (action) {
-            case "Search":
-                cusModel.search();
+            case "Add To Trolley":
+                cusModel.doAdd((Product) object[0], (int) object[1] );
                 break;
-            case "Add to Trolley":
-                cusModel.addToTrolley();
-                break;
-            case "Cancel":
-                cusModel.cancel();
-                break;
-            case "Check Out":
-                cusModel.checkOut();
-                break;
-            case "OK & Close":
-                cusModel.closeReceipt();
+            case "Remove From Trolley":
+                cusModel.doRemove((Product) object[0]);
                 break;
         }
     }
-
+    /**
+     * dispatches customer action.
+     * @param action The name of the action to perform.
+     * @throws SQLException if a database operation fails.
+     * @throws IOException  if an I/O operation fails.
+     */
+    public void doAction(String action) throws SQLException, IOException {
+        switch (action) {
+            case "Search":
+                cusModel.doSearch();
+                break;
+            case "Cancel":
+                cusModel.doCancel();
+                break;
+            case "Clear":
+                cusModel.doClear();
+                break;
+            case "Check Out":
+                cusModel.doCheckOut();
+                break;
+            case "OK & Close":
+                cusModel.doCloseReceipt();
+                break;
+        }
+    }
 }
