dd34a9297d194aafe7ea10c184d771a1a900561f
Return <131034915+ReturnToNothing@users.noreply.github.com>
2025-12-02 00:10:47 +0000

    ¬ uncommented the productNotifier class

--- src/main/java/ci553/happyshop/client/Main.java
+++ src/main/java/ci553/happyshop/client/Main.java
@@ -80,9 +80,9 @@ public class Main extends Application {
         cusModel.databaseRW = databaseRW;
         cusView.start(new Stage());
 
-        //RemoveProductNotifier removeProductNotifier = new RemoveProductNotifier();
-        //removeProductNotifier.cusView = cusView;
-        //cusModel.removeProductNotifier = removeProductNotifier;
+        RemoveProductNotifier removeProductNotifier = new RemoveProductNotifier();
+        removeProductNotifier.cusView = cusView;
+        cusModel.proNotifier = removeProductNotifier;
     }
 
     /** The picker GUI, - for staff to pack customer's order,
